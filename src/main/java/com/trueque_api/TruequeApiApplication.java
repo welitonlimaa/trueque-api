5394a4f3a3be1c890d5b19a51aba1795089c9e27
Weliton Pereira Lima Junior <welitonlimadev@gmail.com>
2025-04-05 13:17:01 -0300

    main

---
+++ src/main/java/com/trueque_api/TruequeApiApplication.java
@@ -0,0 +1,13 @@
+package com.trueque_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruequeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruequeApiApplication.class, args);
+	}
+
+}
