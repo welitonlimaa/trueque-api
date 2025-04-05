ae887c6264144bf113410868b5401fb26dac2eb3
Weliton Pereira Lima Junior <welitonlimadev@gmail.com>
2025-04-05 13:21:10 -0300

    test

---
+++ src/test/java/com/example/trueque_api/TruequeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trueque_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruequeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
