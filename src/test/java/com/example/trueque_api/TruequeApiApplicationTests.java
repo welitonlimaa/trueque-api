07f3f86b50b992309b0f67b1c34d6a8d772d2364
Weliton Pereira Lima Junior <welitonlimadev@gmail.com>
2025-05-05 18:01:47 -0300

    test

--- src/test/java/com/example/trueque_api/TruequeApiApplicationTests.java
+++ src/test/java/com/example/trueque_api/TruequeApiApplicationTests.java
@@ -1,13 +1,15 @@
 package com.example.trueque_api;
 
+import com.trueque_api.TruequeApiApplication;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = TruequeApiApplication.class)
+@ActiveProfiles("test")
 class TruequeApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
