715a5ac0f4f082ac6633c5f2d49d5dc25b3c0ec1
Weliton Pereira Lima Junior <welitonlimadev@gmail.com>
2025-05-05 18:06:25 -0300

    feat: trade offer repository

---
+++ src/main/java/com/trueque_api/staff/repository/TradeOfferRepository.java
@@ -0,0 +1,11 @@
+// package com.trueque_api.staff.repository;
+
+// import com.trueque_api.staff.model.TradeOffer;
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+// import java.util.List;
+// import java.util.UUID;
+
+// public interface TradeOfferRepository extends JpaRepository<TradeOffer, UUID> {
+//     List<TradeOffer> findAllByOfferedByIdOrReceivedById(UUID offeredById, UUID receivedById);
+// }
