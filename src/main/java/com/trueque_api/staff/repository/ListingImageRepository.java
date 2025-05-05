5b9bef2e661cb9aebf12ed084aa169d51ab0b676
Weliton Pereira Lima Junior <welitonlimadev@gmail.com>
2025-05-05 18:05:22 -0300

    feat: listing repository

---
+++ src/main/java/com/trueque_api/staff/repository/ListingImageRepository.java
@@ -0,0 +1,11 @@
+// package com.trueque_api.staff.repository;
+
+// import com.trueque_api.staff.model.ListingImage;
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+// import java.util.List;
+// import java.util.UUID;
+
+// public interface ListingImageRepository extends JpaRepository<ListingImage, UUID> {
+//     List<ListingImage> findByListingId(UUID listingId);
+// }
---
+++ src/main/java/com/trueque_api/staff/repository/ListingRepository.java
@@ -0,0 +1,11 @@
+// package com.trueque_api.staff.repository;
+
+// import com.trueque_api.staff.model.Listing;
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+// import java.util.List;
+// import java.util.UUID;
+
+// public interface ListingRepository extends JpaRepository<Listing, UUID> {
+//     List<Listing> findAllByUserId(UUID userId);
+// }
