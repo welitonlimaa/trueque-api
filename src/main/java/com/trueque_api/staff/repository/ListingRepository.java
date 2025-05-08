e8b3a3c0dc16bab774892b46786c92394181aaf8
Weliton Pereira Lima Junior <welitonlimadev@gmail.com>
2025-05-08 20:03:05 -0300

    refactor: listing repository

--- src/main/java/com/trueque_api/staff/repository/ListingRepository.java
+++ src/main/java/com/trueque_api/staff/repository/ListingRepository.java
@@ -1,11 +1,11 @@
-// package com.trueque_api.staff.repository;
+package com.trueque_api.staff.repository;
 
-// import com.trueque_api.staff.model.Listing;
-// import org.springframework.data.jpa.repository.JpaRepository;
+import com.trueque_api.staff.model.Listing;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-// import java.util.List;
-// import java.util.UUID;
+import java.util.List;
+import java.util.UUID;
 
-// public interface ListingRepository extends JpaRepository<Listing, UUID> {
-//     List<Listing> findAllByUserId(UUID userId);
-// }
+public interface ListingRepository extends JpaRepository<Listing, UUID> {
+    List<Listing> findAllByUserId(UUID userId);
+}
