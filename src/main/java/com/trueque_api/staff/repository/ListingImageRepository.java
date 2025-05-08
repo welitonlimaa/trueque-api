af9a577d4bc3e2ccdca79aa31941e6ff5ca7cf78
Weliton Pereira Lima Junior <welitonlimadev@gmail.com>
2025-05-08 20:03:48 -0300

    refactor listings imgs

--- src/main/java/com/trueque_api/staff/repository/ListingImageRepository.java
+++ src/main/java/com/trueque_api/staff/repository/ListingImageRepository.java
@@ -1,11 +1,11 @@
-// package com.trueque_api.staff.repository;
+package com.trueque_api.staff.repository;
 
-// import com.trueque_api.staff.model.ListingImage;
-// import org.springframework.data.jpa.repository.JpaRepository;
+import com.trueque_api.staff.model.ListingImage;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-// import java.util.List;
-// import java.util.UUID;
+import java.util.List;
+import java.util.UUID;
 
-// public interface ListingImageRepository extends JpaRepository<ListingImage, UUID> {
-//     List<ListingImage> findByListingId(UUID listingId);
-// }
+public interface ListingImageRepository extends JpaRepository<ListingImage, UUID> {
+    List<ListingImage> findByListingId(UUID listingId);
+}
