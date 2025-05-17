a96d8fd67357afdef3e648e63906f19bc0bcfe2e
Weliton Pereira Lima Junior <welitonlimadev@gmail.com>
2025-05-17 11:47:47 -0300

    refactor: listing repository

--- src/main/java/com/trueque_api/staff/repository/ListingRepository.java
+++ src/main/java/com/trueque_api/staff/repository/ListingRepository.java
@@ -1,11 +1,25 @@
 package com.trueque_api.staff.repository;
 
 import com.trueque_api.staff.model.Listing;
+
+import jakarta.persistence.LockModeType;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ListingRepository extends JpaRepository<Listing, UUID> {
+    @Query("SELECT l FROM Listing l WHERE l.id = :listingId AND l.user.email = :email")
+    Optional<Listing> findByIdAndUserEmail(UUID listingId, String email);
+
     List<Listing> findAllByUserId(UUID userId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT l FROM Listing l WHERE l.id = :id")
+    Optional<Listing> findByIdForUpdate(@Param("id") UUID id);
 }
