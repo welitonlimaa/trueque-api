eb16ade8ddea501d486725d30e44e7ffa2b21000
Weliton Pereira Lima Junior <welitonlimadev@gmail.com>
2025-05-17 11:43:13 -0300

    refactor: login request dto

--- src/main/java/com/trueque_api/staff/dto/LoginRequestDTO.java
+++ src/main/java/com/trueque_api/staff/dto/LoginRequestDTO.java
@@ -1,11 +1,18 @@
 package com.trueque_api.staff.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class LoginRequestDTO {
+
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Formato de email inválido")
     private String email;
+
+    @NotBlank(message = "Senha é obrigatória")
     private String password;
 }
