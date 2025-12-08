15caeebe07b4b365acce60583535801545cf8d12
Weliton Pereira Lima Junior <welitonlimadev@gmail.com>
2025-12-08 15:27:04 -0300

    feat: cors

--- src/main/java/com/trueque_api/staff/config/SecurityConfig.java
+++ src/main/java/com/trueque_api/staff/config/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .cors(cors -> {}) 
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
