47f6dc1198cff0cf7ea6092022ea5c05e091bade
Weliton Pereira Lima Junior <welitonlimadev@gmail.com>
2026-02-03 11:14:50 -0300

    refactor: cors

--- src/main/java/com/trueque_api/staff/config/CorsConfig.java
+++ src/main/java/com/trueque_api/staff/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173", "http://192.168.0.106:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("*")
