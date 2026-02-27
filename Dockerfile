cf3ecd7642df5d40fa464fe71dbb7c7078dbcdaa
Weliton Pereira Lima Junior <welitonlimadev@gmail.com>
2026-02-27 14:27:00 -0300

    refactor: deploy

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM maven:3.9.5-eclipse-temurin-21 AS builder
-WORKDIR /app
-COPY . .
-RUN mvn clean package -DskipTests
-
 FROM eclipse-temurin:21-jdk-alpine
+
 WORKDIR /app
-COPY --from=builder /app/target/*.jar app.jar
-ENTRYPOINT ["java", "-jar", "app.jar"]
+
+COPY target/*.jar app.jar
+
+EXPOSE 8080
+
+ENTRYPOINT ["java","-jar","app.jar"]
--- src/main/java/com/trueque_api/staff/config/CorsConfig.java
+++ src/main/java/com/trueque_api/staff/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://192.168.0.106:5173")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("*")
